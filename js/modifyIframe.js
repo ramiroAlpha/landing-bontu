591139f138181415a15faa35e3c8a598edd22041
Carlos Añorve <carlos.anorve@alphacredit.mx>
2019-09-26 12:15:38 -0500

    desarrollando ando

--- js/modifyIframe.js
+++ js/modifyIframe.js
@@ -1,1 +1,39 @@
-$("#frameID").attr("src", "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0");
+
+
+// $("#frameID").attr("src", "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0");
+
+// var video = $('<video />', {
+//     id: 'video',
+//     src: 'http://video-js.zencoder.com/oceans-clip.mp4',
+//     type: 'video/mp4',
+//     controls: true
+// });
+// video.appendTo($('#gallery'));
+
+
+// var jsonStructure = {
+
+// }
+
+var videoSection = $("#divVideo")
+
+
+// var divVideo = $("<div/>", {
+//     class: "embed-responsive embed-responsive-16by9",
+//     id: "divVideo"
+// })
+
+
+var videoFrame = $("<iFrame/>", {
+    id: "video",
+    src: "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0",
+    class: "embed-responsive-item",
+    allowfullscreen: true
+});
+
+// videoFrame.appendTo($(divVideo))
+
+videoFrame.appendTo($(videoSection))
+
+
+
