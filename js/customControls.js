8a1b1d911ab8fd862af39b7875dc3a654061a4ab
Carlos Añorve <carlos.anorve@alphacredit.mx>
2019-09-26 14:43:02 -0500

    se agrega la funcionalidad de abstracción de json

--- js/customControls.js
+++ js/customControls.js
@@ -1,52 +1,58 @@
-<script>
-    var jsonCustom =
-        [
-                {
-        control: 'input',
-    placeholder: 'Enter name',
-    maxlength: 10,
-    minlength: 1,
-    id: 'nameId',
-    name: 'nameCustom',
-    type: 'text',
-    class: "form-control form-control-sm",
-    isRequired: true
-},
-                {
-        control: 'input',
-    placeholder: 'Enter name',
-    maxlength: 10,
-    minlength: 1,
-    id: 'name2',
-    name: 'name2',
-    type: 'password',
-    class: "form-control form-control-sm",
-    isRequired: true
+var videoUno = {
+    control: 'iFrame',
+    id: "video",
+    src: "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0",
+    class: "embed-responsive-item",
+    allowfullscreen: true
 }
-];
 
+var jsonCustom = {
+    myCustom:
+        [
+            {
+                control: 'input',
+                placeholder: 'Enter name',
+                maxlength: 10,
+                minlength: 1,
+                id: 'nameId',
+                name: 'nameCustom',
+                type: 'text',
+                class: "form-control form-control-sm",
+                isRequired: true
+            },
+            {
+                control: 'input',
+                placeholder: 'Enter name',
+                maxlength: 10,
+                minlength: 1,
+                id: 'name2',
+                name: 'name2',
+                type: 'password',
+                class: "form-control form-control-sm",
+                isRequired: true
+            },
 
-        $(document).ready(function () {
-        myFunction(jsonCustom);
+        ],
+    divVideo:
+        [
+            videoUno
+        ]
+};
+
+$(document).ready(function () {
+    for (div in jsonCustom) {
+        myFunction(jsonCustom[div], div);
+    }
 });
 
 
-        function myFunction(jsonCustom) {
-        jsonCustom.forEach(element => {
-            console.log(element);
-            var inputElement = document.createElement(element.control);
-            switch (element.control) {
-                case "input":
-                    $(inputElement).attr("placeholder", element.placeholder)
-                    $(inputElement).attr("type", element.type)
-                    $(inputElement).attr("maxlength", element.maxlength)
-                    $(inputElement).attr("minlength", element.minlength)
-                    $(inputElement).attr("required", element.isRequired)
-                    $(inputElement).attr("class", element.class)
-                    break;
-            }
-            document.getElementById("myCustom").appendChild(inputElement);
-        });
+function myFunction(elementos, divId) {
+    elementos.forEach(element => {
+        var inputElement = document.createElement(element.control);
+        for (e in element) {
+            $(inputElement).attr(e, element[e])
+        }
+        document.getElementById(divId).appendChild(inputElement);
+    });
 }
 
-    </script>
--- js/modifyIframe.js
+++ js/modifyIframe.js
@@ -1,39 +1,13 @@
+// var videoSection = $("#divVideo")
 
 
-// $("#frameID").attr("src", "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0");
-
-// var video = $('<video />', {
-//     id: 'video',
-//     src: 'http://video-js.zencoder.com/oceans-clip.mp4',
-//     type: 'video/mp4',
-//     controls: true
+// var videoFrame = $("<iFrame/>", {
+//     id: "video",
+//     src: "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0",
+//     class: "embed-responsive-item",
+//     allowfullscreen: true
 // });
-// video.appendTo($('#gallery'));
 
 
-// var jsonStructure = {
-
-// }
-
-var videoSection = $("#divVideo")
-
-
-// var divVideo = $("<div/>", {
-//     class: "embed-responsive embed-responsive-16by9",
-//     id: "divVideo"
-// })
-
-
-var videoFrame = $("<iFrame/>", {
-    id: "video",
-    src: "https://www.youtube.com/embed/cBusDEvg9Tw?controls=0",
-    class: "embed-responsive-item",
-    allowfullscreen: true
-});
-
-// videoFrame.appendTo($(divVideo))
-
-videoFrame.appendTo($(videoSection))
-
-
+// videoFrame.appendTo($(videoSection))
 
