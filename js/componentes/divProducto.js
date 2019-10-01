6880a4141ce5234531c08fa8a546df8c6f4d0a07
Carlos Añorve <carlos.anorve@alphacredit.mx>
2019-10-01 10:44:05 -0500

    Liberación final

--- js/componentes/divProducto.js
+++ js/componentes/divProducto.js
@@ -1,4 +1,3 @@
-// div1 = basicStructure('div')
 div2 = basicStructure('div')
 div3 = basicStructure('div')
 div4 = basicStructure('div')
@@ -6,8 +5,6 @@ div5 = basicStructure('div')
 div6 = basicStructure('div')
 div7 = basicStructure('div')
 div8 = basicStructure('div')
-
-
 
 img1 = basicStructure('img')
 img2 = basicStructure('img')
@@ -21,15 +18,6 @@ p1 = basicStructure('p')
 p2 = basicStructure('p')
 p3 = basicStructure('p')
 
-
-
-
-
-// div1.attr.class = 'container-container-fluid rect-bg max'
-// div1.attr.id = 'producto'
-// div1.childComponent['div1'] = div2
-
-
 div2.attr.class = 'row'
 div2.childComponent['div3'] = div3
 div2.childComponent['div4'] = div4
@@ -37,14 +25,11 @@ div2.childComponent['div8'] = div8
 
 div3.attr.class = 'col-sm-2'
 
-
 div4.attr.class = 'col-sm-8 row rects'
 div4.childComponent['div5'] = div5
 div4.childComponent['div6'] = div6
 div4.childComponent['div7'] = div7
 
-
-//Div5
 div5.attr.class = 'col-sm-4'
 div5.childComponent['img1'] = img1
 div5.childComponent['h3_prod1'] = h3_prod1
@@ -56,8 +41,6 @@ img1.attr.alt="Appointment"
 h3_prod1.text['texto'] = "A tu medida"
 
 p1.text['texto'] = "Crédito basado en tu ciclo de ventas. Obtén el dinero que necesitas, a un plazo de hasta 12 meses"
-
-//Div6
 
 div6.attr.class = 'col-sm-4'
 div6.childComponent['img2'] = img2
@@ -71,8 +54,6 @@ h3_prod2.text['texto'] = "Accesible"
 
 p2.text['texto'] = "Obtén tu crédito de forma rápida y sencilla, todo online. Así no hay papeleo ni haces filas largas"
 
-
-//Div7
 div7.attr.class = 'col-sm-4'
 div7.childComponent['img3'] = img3
 div7.childComponent['h3_prod3'] = h3_prod3
@@ -84,9 +65,6 @@ img3.attr.alt="Clock"
 h3_prod3.text['texto'] = "Inmediato"
 
 p3.text['texto'] = "Te entregamos tu dinero vía transferencia en cuestión de minutos"
-
-
-
 
 addJson2Html({ div2 }, 'producto')
 
